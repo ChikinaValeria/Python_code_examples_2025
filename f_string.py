3419837b584e16e91dc38134d579a01fa31cae0c
Valeria Chikina <dliavajnogo@gmail.com>
2025-09-12 08:34:07 +0300

    Add get_input function

--- f_string.py
+++ f_string.py
@@ -1,27 +1,23 @@
-aloitus = input("Print the selityksen aloitus here: ")
-while aloitus == "":
-    print("Aloitus can't be empty")
-    aloitus = input("Print the selityksen aloitus here: ")
-opettaja = input("Print oppettajan nimi here: ")
-while opettaja == "":
-    print("opettajan nimi can't be empty")
-    opettaja = input("Print oppettajan nimi here: ")
-k_tyyppi = input("Print kotitehtävän tyyppi here: ")
-while k_tyyppi == "":
-    print("kotitehtävän tyyppi can't be empty")
-    k_tyyppi = input("Print kotitehtävän tyyppi here: ")
-l_laji = input("Print lemmikkieläimen laji here: ")
-while l_laji == "":
-    print("lemmikkieläimen laji can't be empty")
-    l_laji = input("Print lemmikkieläimen laji here: ")
-l_nimi = input("Print lemmikkieläimen nimi here: ")
-while l_nimi == "":
-    print("lemmikkieläimen nimi can't be empty")
-    l_nimi = input("Print lemmikkieläimen nimi here: ")
-l_teko = input("Print lemmikkieläimen teko here: ")
-while l_teko == "":
-    print("lemmikkieläimen teko can't be empty")
-    l_teko = input("Print lemmikkieläimen teko here: ")
+def get_input(prompt, error_message):
+    """
+    Prompts the user for input and repeats the request 
+    if the input is empty or consists only of spaces.
+    """
+    user_input = input(prompt)
+    # user_input.strip() возвращает строку, очищенную от пробелов и табов
+    # пустая строка считается False
+    # not False -> True, пока строка пустая, выполняй
+    while not user_input.strip():
+        print(error_message)
+        user_input = input(prompt)
+    return user_input
+
+aloitus = get_input("Print the selityksen aloitus here: ", "Aloitus can't be empty")
+opettaja = get_input("Print oppettajan nimi here: ", "opettajan nimi can't be empty")
+k_tyyppi = get_input("Print kotitehtävän tyyppi here: ", "kotitehtävän tyyppi can't be empty")
+l_laji = get_input("Print lemmikkieläimen laji here: ", "lemmikkieläimen laji can't be empty")
+l_nimi = get_input("Print lemmikkieläimen nimi here: ", "lemmikkieläimen nimi can't be empty")
+l_teko = get_input("Print lemmikkieläimen teko here: ", "lemmikkieläimen teko can't be empty")
 
 print(f'{aloitus}, {opettaja}, en tiedä mihin se {k_tyyppi} oikein joutui! '
       f'Lemmikki{l_laji}ni {l_nimi} afgvarmaankin {l_teko} sen! ')
