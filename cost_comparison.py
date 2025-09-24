511a944189719cc4a33599fdfcae85ac24fe3973
Valeria Chikina <dliavajnogo@gmail.com>
2025-09-24 14:49:34 +0300

    Correct variable names

--- cost_comparison.py
+++ cost_comparison.py
@@ -1,12 +1,12 @@
-ANNUAL_MILEAGE = (float(input("Give your estimated annual mileage: ")))
-GASOLINE_PRICE = (float(input("Give the price of gasoline: ")))
-DIESEL_PRICE = (float(input("Give the price of diesel: ")))
-GASOLINE_CONSUMPTION = (float(input("Give your estimated gasoline consumption (l/100km): ")))
-DIESEL_CONSUMPTION = (float(input("Give your estimated diesel consumption (l/100km): ")))
-DIESEL_TAX = (float(input("Give the diesel fuel tax amount: ")))
+annual_mileage = (float(input("Give your estimated annual mileage: ")))
+gasoline_price = (float(input("Give the price of gasoline: ")))
+diesel_price = (float(input("Give the price of diesel: ")))
+gasoline_comsumption = (float(input("Give your estimated gasoline consumption (l/100km): ")))
+diesel_consumption = (float(input("Give your estimated diesel consumption (l/100km): ")))
+diesel_tax = (float(input("Give the diesel fuel tax amount: ")))
 
-gasoline_annual_cost = round(ANNUAL_MILEAGE/100 * GASOLINE_CONSUMPTION * GASOLINE_PRICE, 2)
-diesel_annual_cost = round(ANNUAL_MILEAGE/100 * DIESEL_CONSUMPTION * DIESEL_PRICE + DIESEL_TAX, 2)
+gasoline_annual_cost = round(annual_mileage/100 * gasoline_comsumption * gasoline_price, 2)
+diesel_annual_cost = round(annual_mileage/100 * diesel_consumption * diesel_price + diesel_tax, 2)
 print(f'gasoline_annual_cost: {gasoline_annual_cost}')
 print(f'diesel_annual_cost: {diesel_annual_cost}')
 
@@ -14,8 +14,10 @@ print(f'The annual travel costs of gasoline-powered car is {gasoline_annual_cost
 print(f'The annual travel costs of diesel-powered car is {diesel_annual_cost}')
 
 if (gasoline_annual_cost > diesel_annual_cost):
-    print(f'The annual travel costs of gasoline-powered car are bigger than costs of diesel-powered car by {gasoline_annual_cost-diesel_annual_cost} euros')
+    print(f'The annual travel costs of gasoline-powered car are bigger\
+          than costs of diesel-powered car by {gasoline_annual_cost-diesel_annual_cost} euros')
 elif(gasoline_annual_cost < diesel_annual_cost):
-    print(f'The annual travel costs of gasoline-powered car are smaller than costs of diesel-powered car by {diesel_annual_cost-gasoline_annual_cost} euros')
+    print(f'The annual travel costs of gasoline-powered car are smaller\
+          than costs of diesel-powered car by {diesel_annual_cost-gasoline_annual_cost} euros')
 else:
     print(f'The annual travel costs of gasoline-powered car are equal to ones of diesel-powered car')        
